1612d0bbd091da9fdde908af31bb87714f247df9
Daniel Chattan <dnchattan@gmail.com>
2023-02-14 18:25:47 -0500

    add resources

--- SampleExtension/UI/Greeter.xaml.cs
+++ SampleExtension/UI/Greeter.xaml.cs
@@ -19,28 +19,44 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 
 using CustomExtensions.WinUI;
+using Microsoft.Windows.ApplicationModel.Resources;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace SampleExtension.UI
 {
-    public sealed partial class Greeter : UserControl
-    {
-        public Greeter()
-        {
-            // this.InitializeComponent();
-            this.LoadComponent(ref _contentLoaded);
-        }
+	public sealed partial class Greeter : UserControl
+	{
+		public Greeter()
+		{
+			// this.InitializeComponent();
+			this.LoadComponent(ref _contentLoaded);
+		}
 
-        public GreetEntity TargetEntity
-        {
-            get { return (GreetEntity)GetValue(TargetEntityProperty); }
-            set { SetValue(TargetEntityProperty, value); }
-        }
+		public GreetEntity TargetEntity
+		{
+			get { return (GreetEntity)GetValue(TargetEntityProperty); }
+			set { SetValue(TargetEntityProperty, value); }
+		}
 
-        // Using a DependencyProperty as the backing store for TargetEntity.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty TargetEntityProperty =
-            DependencyProperty.Register(nameof(TargetEntity), typeof(GreetEntity), typeof(Greeter), new PropertyMetadata(GreetEntity.World));
-    }
+		// Using a DependencyProperty as the backing store for TargetEntity.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty TargetEntityProperty =
+			DependencyProperty.Register(nameof(TargetEntity), typeof(GreetEntity), typeof(Greeter), new PropertyMetadata(GreetEntity.World));
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				// var loader = ResourceLoader.GetForViewIndependentUse(ResourceMapName.Text);
+				var loader = new ResourceLoader(ResourceMapName.Text); //, "SampleExtension.SampleAppExtension/Resources.map.txt");
+				//var loader = global::Windows.ApplicationModel.Resources.ResourceLoader.GetForViewIndependentUse(ResourceMapName.Text);
+				ResourceValue.Text = loader.GetString(ResourceKey.Text);
+			}
+			catch (Exception ex)
+			{
+				ResourceValue.Text = ex.GetType().Name + ": " + ex.Message;
+			}
+		}
+	}
 }
